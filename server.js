a2031388cf522eac318c8f99821ce8d519944bdd
Avineet Yadav <12minute00@gmail.com>
2024-08-07 00:38:02 +0530

    cors

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const port = 4200;
+const frontendUri = "http://localhost:5173"; // Replace with your frontend URL
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", frontendUri);
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
     "Access-Control-Allow-Headers",
